256125c467c05006d390a97c03b309c0ef9d8e8d
LinelinLove <chrisline.lin@outlook.fr>
2024-10-02 15:51:04 +0200

    map graph

--- src/app/components/Graph.tsx
+++ src/app/components/Graph.tsx
@@ -3,7 +3,7 @@ import Chart from "chart.js/auto";
 import { CategoryScale } from "chart.js";
 import { Chart as ChartJS } from "chart.js/auto";
 import { Bar, Doughnut, Line } from "react-chartjs-2";
-
+import sourceData from "../utils/data.json";
 Chart.register(CategoryScale);
 
 const Graph = () => {
@@ -13,11 +13,11 @@ const Graph = () => {
       <div className="dataCard customerCard w-96 h-96">
         <Bar
           data={{
-            labels: ["Paris", "La Rochelle", "New York"],
+            labels: sourceData.map((data) => data.city),
             datasets: [
               {
                 label: "°C",
-                data: [27, 19, 4],
+                data: sourceData.map((data) => data.degree),
               },
             ],
           }}
